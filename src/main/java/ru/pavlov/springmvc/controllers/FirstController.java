9b92cf4086669e559a7cc24b3b52b8fb2d81e2ec
Павлов Станислав Степанович <pavlov_ss@albank.ru>
2023-10-31 16:44:57 +0900

    Create calculator endpoint

--- src/main/java/ru/pavlov/springmvc/controllers/FirstController.java
+++ src/main/java/ru/pavlov/springmvc/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.pavlov.springmvc.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello " + name + " " + surname);
+        // System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
 
         return "first/hello";
     }
@@ -21,5 +24,31 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    public double calculate(double a, double b, String action) {
+        switch (action) {
+            case "multiplication":
+                return a * b;
+            case "addition":
+                return a + b;
+            case "subtraction":
+                return a - b;
+            case "division":
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam("a") double a,
+                                 @RequestParam("b") double b,
+                                 @RequestParam("action") String action,
+                                 Model model) {
+
+        model.addAttribute("result", calculate(a, b, action));
+
+        return "first/calculator";
     }
 }
